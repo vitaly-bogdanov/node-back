c1c2ed8b498a85bf9274f97caa593300f2a1723e
Vitaly <kaliummati@gmail.com>
2021-07-10 17:47:55 +0900

    models added

--- core/companies/companies.constant.js
+++ core/companies/companies.constant.js
@@ -1,1 +1,5 @@
 export const COMPANIES_PATH = '/companies';
+
+export const BUSINESS_ENTITI = { OOO: 'ООО', OAO: 'ОАО' };
+export const TYPE = { AGENT: 'agent', CONSTRACTOR: 'contractor' };
+export const STATUS = { ACTIVE: 'active', INACTIVE: 'inactive' };
